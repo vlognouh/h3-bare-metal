39cdba64aa5d57fea3b77a2322a7e26ad5c07551
Vu Nguyen <nhoangvubk@gmail.com>
2018-05-24 16:06:08 +0700

    Cleanup SPILib & Convert to Unix line ending

--- Libraries/Include/Periph/SpiLib.h
+++ Libraries/Include/Periph/SpiLib.h
@@ -1,46 +1,60 @@
 #ifndef __SPILIB_H__
 #define __SPILIB_H__
 
-#define BIT(nr)			(1UL << (nr))
+#define BIT(nr)         (1UL << (nr))
 #define SPI0_BASE   0x01C68000
 #define SPI1_BASE   0x01C69000
 
-#define SPI_GBL_CTL     0x04
+// #define SPI_GBL_CTL     0x04
 #define SPI_GBL_CTL_BUS_ENABLE  BIT(0)
 #define SPI_GBL_CTL_MASTER      BIT(1)
 #define SPI_GBL_CTL_TP          BIT(7)
 
 #define SPI_TFR_CTL     0x08
-#define SPI_TFR_CTL_CPHA			BIT(0)
-#define SPI_TFR_CTL_CPOL			BIT(1)
-#define SPI_TFR_CTL_SPOL			BIT(2)
-#define SPI_TFR_CTL_CS_MASK			0x30
-#define SPI_TFR_CTL_CS(cs)			(((cs) << 4) & SPI_TFR_CTL_CS_MASK)
-#define SPI_TFR_CTL_CS_MANUAL			BIT(6)
-#define SPI_TFR_CTL_CS_LEVEL			BIT(7)
-#define SPI_TFR_CTL_DHB			BIT(8)
-#define SPI_TFR_CTL_FBS			BIT(12)
-#define SPI_TFR_CTL_SDM			BIT(13)
-#define SPI_TFR_CTL_XCH			BIT(31)
+#define SPI_TFR_CTL_CPHA         BIT(0)
+#define SPI_TFR_CTL_CPOL         BIT(1)
+#define SPI_TFR_CTL_SPOL         BIT(2)
+#define SPI_TFR_CTL_CS_MASK         0x30
+#define SPI_TFR_CTL_CS(cs)       (((cs) << 4) & SPI_TFR_CTL_CS_MASK)
+#define SPI_TFR_CTL_CS_MANUAL       BIT(6)
+#define SPI_TFR_CTL_CS_LEVEL        BIT(7)
+#define SPI_TFR_CTL_DHB       BIT(8)
+#define SPI_TFR_CTL_FBS       BIT(12)
+#define SPI_TFR_CTL_SDM       BIT(13)
+#define SPI_TFR_CTL_XCH       BIT(31)
 
-#define SPI_INT_CTL         0x10
-#define SPI_INT_STA         0x14
-#define SPI_FIFO_CTL        0x18
-#define SPI_FIFO_STA        0x1C
-#define SPI_WCR             0x20
-#define SPI_CLK_CTL         0x24
+#define SPI_RXFIFO_RESET   BIT(15)
+#define SPI_TXFIFO_RESET   BIT(31)
 
-#define SPI_BURST_CNT       0x30
-#define SPI_XMIT_CNT        0x34
-#define SPI_BURST_CTL_CNT   0x38
-#define SPI_TXDATA     0x200
-#define SPI_RXDATA     0x300
+typedef struct {
+   uint32_t RESERVED0;
+   volatile uint32_t SPI_GBL;          // 0x04
+   volatile uint32_t SPI_TCR;          // 0x08
+   uint32_t RESERVED1;
+   volatile uint32_t SPI_INT_CTL;      // 0x10
+   volatile uint32_t SPI_INT_STA;      // 0x14
+   volatile uint32_t SPI_FIFO_CTL;     // 0x18
+   volatile uint32_t SPI_FIFO_STA;     // 0x1C
+   volatile uint32_t SPI_WCR;          // 0x20
+   volatile uint32_t SPI_CLK_CTL;      // 0x24
+   uint32_t RESERVED2[2];
+   volatile uint32_t SPI_BURST_CNT;
+   volatile uint32_t SPI_XMIT_CNT;
+   volatile uint32_t SPI_BURST_CTL_CNT;
+   uint32_t RESERVED3[113];
+   volatile uint8_t SPI_TXDATA;
+   uint8_t  RESERVED4[3];
+   volatile uint32_t SPI_RXDATA;
+} spi_reg_t;
+
+#define SPI0 ((spi_reg_t *)(0x01C68000))
+#define SPI1 ((spi_reg_t *)0x01C69000)
 
 void SPI0_Init(void);
 void     CE_Low(void);
 void     CE_High(void);
-void     SS0_Low(void);
-void     SS0_High(void);
+void     CS0_Low(void);
+void     CS0_High(void);
 uint8_t SPI0_Read_Byte(uint8_t Address);
 uint8_t SPI0_Read_Reg(uint8_t Reg);
 void delay(void);
--- Libraries/Periph/SpiLib.c
+++ Libraries/Periph/SpiLib.c
@@ -37,197 +37,107 @@ void CE_Low(void)
     WriteReg(GPIO_BASE, PA_DATA_OFFSET, Temp & ~(1 << 7));
 }
 
-void SS0_High(void)
+void CS0_High(void)
 {
     // PC3 High
-//    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-//    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp | 1 << 7);
+//    Temp = SPI0->SPI_TCR;
+//    SPI0->SPI_TCR = Temp | 1 << 7;
     Temp = ReadReg(GPIO_BASE, PC_DATA_OFFSET);
     WriteReg(GPIO_BASE, PC_DATA_OFFSET, Temp | 1 << 3);
 }
 
-void SS0_Low(void)
+void CS0_Low(void)
 {
     // PC3 Low
-//    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-//    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp & ~(1 << 7));
+//    Temp = SPI0->SPI_TCR);
+//    SPI0->SPI_TCR = Temp & ~(1 << 7);
     Temp = ReadReg(GPIO_BASE, PC_DATA_OFFSET);
     WriteReg(GPIO_BASE, PC_DATA_OFFSET, Temp & ~(1 << 3));
 }
 void delay(void)
 {
     int i;
-    for(i = 0; i<100000; i++)
+    for (i = 0; i < 100000; i++)
     {;}
 }
 
 void Dummy_Byte(void)
 {
+    CS0_Low();
+
     // Clear all pending interrupt
-    WriteReg(SPI0_BASE, SPI_INT_STA, ~0);
+    SPI0->SPI_INT_STA = ~0;
 
     // Reset FIFO
-    WriteReg(SPI0_BASE, SPI_FIFO_CTL, 1 << 31 | 1 << 15);
+    // SPI0->SPI_FIFO_CTL, 1 << 31 | 1 << 15);
+    SPI0->SPI_FIFO_CTL = 1 << 31 | 1 << 15;
 
-    // Setup FIFO Control
-    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-    Temp &= ~SPI_TFR_CTL_CPHA;
-    Temp &= ~SPI_TFR_CTL_CPOL;
-    Temp &= ~SPI_TFR_CTL_FBS;
-    Temp &= ~SPI_TFR_CTL_DHB;
-    Temp |= SPI_TFR_CTL_CS_MANUAL;
-    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp);
-
-    // Setup Clock
-    WriteReg(SPI0_BASE, SPI_CLK_CTL, 1 << 12 | 14);
-
-    // Enable TXFIFO Write Enable
-    Temp = ReadReg(SPI0_BASE, SPI_FIFO_STA);
-    WriteReg(SPI0_BASE, SPI_FIFO_STA, Temp | 1 << 31);
+    // Enable TXFIFO Write
+    SPI0->SPI_FIFO_STA |= 1 << 31;
 
     // Setup counter
-    WriteReg(SPI0_BASE, SPI_BURST_CNT, 1);
-    WriteReg(SPI0_BASE, SPI_XMIT_CNT, 1);
-    WriteReg(SPI0_BASE, SPI_BURST_CTL_CNT, 1);
+    SPI0->SPI_BURST_CNT = 1;
+    SPI0->SPI_XMIT_CNT = 1;
+    SPI0->SPI_BURST_CTL_CNT = 1;
 
     // Fill FIFO
-    WriteByte(SPI0_BASE, SPI_TXDATA, 0x00);
-
-    // Enable interrupt
-    WriteReg(SPI0_BASE, SPI_INT_CTL, 1 << 12);
+    SPI0->SPI_TXDATA = 0x00;
 
     // Start Transfer
-    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp | 1 << 31);
+    SPI0->SPI_TCR |= 1 << 31;
 
-    // Wait util TC Flag set
-    while (!(ReadReg(SPI0_BASE, SPI_INT_STA) & (1<<12)));
+    // Wait until TC Flag set
+    while (!(SPI0->SPI_INT_STA & (1 << 12)));
 
     // Clear Flag by write 1 to this bit
-    WriteReg(SPI0_BASE, SPI_INT_STA, 1 << 12);
+    SPI0->SPI_INT_STA = 1 << 12;
 
     // Read RX FIFO
-    Data = ReadByte(SPI0_BASE, SPI_RXDATA);
+    Data = SPI0->SPI_RXDATA;
 
-    //
-    // WriteReg(SPI0_BASE, SPI_INT_CTL, 0);
+    // SPI0->SPI_INT_CTL, 0);
+    CS0_High();
 }
 
 uint8_t Transfer_One(uint8_t Addr)
 {
     // Clear all pending interrupt
-    WriteReg(SPI0_BASE, SPI_INT_STA, ~0);
+    SPI0->SPI_INT_STA = ~0;
 
     // Reset FIFO
-    WriteReg(SPI0_BASE, SPI_FIFO_CTL, 1 << 31 | 1 << 15);
+    SPI0->SPI_FIFO_CTL = 1 << 31 | 1 << 15;
 
-    // Setup FIFO Control
-    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-    Temp &= ~SPI_TFR_CTL_CPHA;
-    Temp &= ~SPI_TFR_CTL_CPOL;
-    Temp &= ~SPI_TFR_CTL_FBS;
-    Temp &= ~SPI_TFR_CTL_DHB;
-    Temp |= SPI_TFR_CTL_CS_MANUAL;
-    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp);
-
-//    // Setup SPI0 PIN MUX PC0, PC1, PC2 | PC3 as output
-//    Temp = ReadReg(GPIO_BASE, PC_CONFIG0_OFFSET);
-//    WriteReg(GPIO_BASE, PC_CONFIG0_OFFSET, Temp & ~(6 << 12) & ~(4 << 8) & ~(4 << 4) & ~4);
-
-    // Setup Clock
-    WriteReg(SPI0_BASE, SPI_CLK_CTL, 1 << 12 | 14);
-
-    // Enable TXFIFO Write Enable
-    Temp = ReadReg(SPI0_BASE, SPI_FIFO_STA);
-    WriteReg(SPI0_BASE, SPI_FIFO_STA, Temp | 1 << 31);
+    // Enable TXFIFO Write
+    SPI0->SPI_FIFO_STA |= 1 << 31;
 
     // Setup counter
-    WriteReg(SPI0_BASE, SPI_BURST_CNT, 1);
-    WriteReg(SPI0_BASE, SPI_XMIT_CNT, 1);
-    WriteReg(SPI0_BASE, SPI_BURST_CTL_CNT, 1);
+    SPI0->SPI_BURST_CNT = 1;
+    SPI0->SPI_XMIT_CNT = 1;
+    SPI0->SPI_BURST_CTL_CNT = 1;
 
     // Fill FIFO
-    WriteByte(SPI0_BASE, SPI_TXDATA, Addr);
-    //WriteByte(SPI0_BASE, SPI_TXDATA, 0x00);
+    SPI0->SPI_TXDATA = Addr;
 
     // Count TX FIFO
-    Temp = ReadReg(SPI0_BASE, SPI_FIFO_STA)>>16 & 0xFF;
-    // printf("--- TX COUNT= 0x%lx\r\n", Temp);
-
-    // Enable interrupt
-    // WriteReg(SPI0_BASE, SPI_INT_CTL, 1 << 12);
+    Temp = SPI0->SPI_FIFO_STA >> 16 & 0xFF;
+    printf("--- TX COUNT= 0x%lx\r\n", Temp);
 
     // Start Transfer
-    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp | 1 << 31);
+    SPI0->SPI_TCR |= 1 << 31;
 
-    // Wait util TC Flag set
-    while (!(ReadReg(SPI0_BASE, SPI_INT_STA) & (1<<12)));
+    // Wait until TC Flag set
+    while (!((SPI0->SPI_INT_STA) & (1 << 12)));
 
     // Clear Flag by write 1 to this bit
-    WriteReg(SPI0_BASE, SPI_INT_STA, 1 << 12);
+    SPI0->SPI_INT_STA = 1 << 12;
 
     // Count RX FIFO
-    // Temp = ReadByte(SPI0_BASE, SPI_FIFO_STA);
-    // printf("--- RX COUNT= 0x%lx\r\n", Temp);
+    Temp = SPI0->SPI_FIFO_STA;
+    printf("--- RX COUNT= 0x%lx\r\n", Temp);
 
-    // // Read RX FIFO
-    // Data = ReadByte(SPI0_BASE, SPI_RXDATA);
-    // printf("--- RX DATA= 0x%x\r\n", Data);
+    Data = SPI0->SPI_RXDATA;
 
-    Data = ReadByte(SPI0_BASE, SPI_RXDATA);
-
-    // //
-    // WriteReg(SPI0_BASE, SPI_INT_CTL, 0);
-
-//    SS0_High();
     return Data;
-}
-
-uint8_t SPI0_Read_Byte(uint8_t Address)
-{
-    // Reset FIFO
-    WriteReg(SPI0_BASE, SPI_FIFO_CTL, 1 << 15 | 1 << 31);
-
-    // Setup Burst count
-    WriteReg(SPI0_BASE, SPI_BURST_CNT, LEN);
-    WriteReg(SPI0_BASE, SPI_XMIT_CNT, LEN);
-    WriteReg(SPI0_BASE, SPI_BURST_CTL_CNT, LEN);
-
-    // Wait
-    while (ReadReg(SPI0_BASE, SPI_TFR_CTL) & (1 << 31));
-
-    // Write TX FIFO
-    WriteByte(SPI0_BASE, SPI_TXDATA, Address);
-    delay();
-
-    // Count TX FIFO
-//    Temp = ReadReg(SPI0_BASE, SPI_FIFO_STA)>>16 & 0xFF;
-//    printf("--- TX COUNT= 0x%x\r\n", Temp);
-
-
-    // Transmit
-    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp | (1 << 31));
-
-    // Wait
-    while (ReadReg(SPI0_BASE, SPI_TFR_CTL) & (1 << 31));
-
-    // Count RX FIFO
-//    Temp = ReadByte(SPI0_BASE, SPI_FIFO_STA);
-//    printf("--- RX COUNT= 0x%x\r\n", Temp);
-    delay();
-    return ReadByte(SPI0_BASE, SPI_RXDATA);
-}
-
-uint8_t SPI0_Read_Reg(uint8_t Reg)
-{
-    uint8_t value;
-    SS0_Low();
-    SPI0_Read_Byte(Reg);
-    value = SPI0_Read_Byte(0x00);
-    SS0_High();
-    return value;
 }
 
 void SPI0_Init(void)
@@ -240,7 +150,7 @@ void SPI0_Init(void)
     WriteReg(CCU_BASE, BUS_CLK_GATING_REG0, Temp | SPI0_GATING);
 
     // Setup BUS SPI0 SCLK 30Mhz=120Mhz/2^2/(4+1)
-   //WriteReg(CCU_BASE, BUS_SPI0_CLK_REG, SPI_SCLK_GATING | 2 << 24 | 2 << 16 | 4 );
+    //WriteReg(CCU_BASE, BUS_SPI0_CLK_REG, SPI_SCLK_GATING | 2 << 24 | 2 << 16 | 4 );
     WriteReg(CCU_BASE, BUS_SPI0_CLK_REG, SPI_SCLK_GATING);
 
     // De-assert SW Reset
@@ -248,25 +158,32 @@ void SPI0_Init(void)
     WriteReg(CCU_BASE, BUS_SW_RESET_REG0, Temp | BUS_SPI0_RESET);
 
     // Setup Global Control: Enter Master mode
-    WriteReg(SPI0_BASE, SPI_GBL_CTL, 1 << 31 | SPI_GBL_CTL_BUS_ENABLE | SPI_GBL_CTL_MASTER | SPI_GBL_CTL_TP);
+    SPI0->SPI_GBL = 1 << 31 | SPI_GBL_CTL_BUS_ENABLE | SPI_GBL_CTL_MASTER | SPI_GBL_CTL_TP;
 
     // Wait until Reset is done
-    while (ReadReg(SPI0_BASE, SPI_GBL_CTL) & 1 << 31);
+    while (SPI0->SPI_GBL & 1 << 31);
 
-//    // Setup transfer control: CPOL, CPHA, MSB_FIRST, SS_SEL = 0b00
-//    Temp = ReadReg(SPI0_BASE, SPI_TFR_CTL);
-//    Temp &= 0;
-////    Temp |= SPI_TFR_CTL_CPHA;
-////    Temp |= SPI_TFR_CTL_CPOL;
-////    Temp |= SPI_TFR_CTL_FBS;
-////    Temp |= SPI_TFR_CTL_DHB;
-//    Temp |= 1 << 13;
-//    Temp |= SPI_TFR_CTL_CS_MANUAL;
-//    WriteReg(SPI0_BASE, SPI_TFR_CTL, Temp);
-//
-//    // Setup Clock control, 24MHz/2^5 = 938KHz
-//    WriteReg(SPI0_BASE, SPI_CLK_CTL, 2 << 8);
+    // Clear all pending interrupt
+    SPI0->SPI_INT_STA = ~0;
+
+    // Reset FIFO
+    SPI0->SPI_FIFO_CTL = SPI_RXFIFO_RESET | SPI_TXFIFO_RESET;
+
+    // Setup FIFO Control
+    Temp = SPI0->SPI_TCR;
+    Temp &= ~SPI_TFR_CTL_CPHA;
+    Temp &= ~SPI_TFR_CTL_CPOL;
+    Temp &= ~SPI_TFR_CTL_FBS;
+    Temp &= ~SPI_TFR_CTL_DHB;
+    Temp |= SPI_TFR_CTL_CS_MANUAL;
+    SPI0->SPI_TCR = Temp;
+
+    // Setup SCLK Clock
+    SPI0->SPI_CLK_CTL = 1 << 12 | 14;
+
+    // Enable interrupt
+    SPI0->SPI_INT_CTL = 1 << 12;
 
     // Wait state
-    WriteReg(SPI0_BASE, 0x20, 20);
+    SPI0->SPI_WCR = 20;
 }
